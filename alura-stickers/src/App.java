87075edd6a58d7d1192d3c71b5736f4af5f1d40d
Raphael Feitosa <raphaelcs2@gmail.com>
2022-07-20 23:16:37 -0300

    Removendo espaços do titulo e adicionando path completo

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -6,20 +6,20 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 
-        // fazer uma conexão HTTP e buscar os top 250 filmes
-
-        // String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
-        // String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java/api/TopMovies.json";
-        // ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
-
-        // String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
+//        fazer uma conexão HTTP e buscar os top 250 filmes
+//
+//        String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
+//        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java/api/TopMovies.json";
+//        ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
+//
+//        String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java/api/NASA-APOD.json";
         ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
 
         var http = new ClienteHttp();
         String json = http.buscaDados(url);
 
-        // exibir e manipular os dados 
+        // exibir e manipular os dados
         List<Conteudo> conteudos = extrator.extraiConteudos(json);
 
         var geradora = new GeradoraDeFigurinhas();
@@ -27,14 +27,18 @@ public class App {
         for (int i = 0; i < 3; i++) {
 
             Conteudo conteudo = conteudos.get(i);
-
+            String conteudoModificado = conteudo
+                    .getTitulo()
+                    .replaceAll(" ", "-");
             InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
-            String nomeArquivo = "saida/" + conteudo.getTitulo() + ".png";
+            String path = "/home/raphael/Spring-Boot/imersao-java/alura-stickers/saida/";
+            String nomeArquivo = path + conteudoModificado + ".png";
 
             geradora.cria(inputStream, nomeArquivo);
 
-            System.out.println(conteudo.getTitulo());
+            System.out.println(nomeArquivo);
             System.out.println();
         }
     }
 }
+
